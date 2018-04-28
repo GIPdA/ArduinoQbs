6e52a0e43c7ed28d27cab7d0bad56957b950ba73
Benjamin Balga <balga.benjamin@gmail.com>
2018-04-28 20:11:19 +0200

    Remove qtcreator plugin from repo (will be in QtC 4.7)

--- qtcreator/plugins/serialterminal/serialterminal_global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QtGlobal>
-
-#if defined(SERIALTERMINAL_LIBRARY)
-#  define SERIALTERMINALSHARED_EXPORT Q_DECL_EXPORT
-#else
-#  define SERIALTERMINALSHARED_EXPORT Q_DECL_IMPORT
-#endif
